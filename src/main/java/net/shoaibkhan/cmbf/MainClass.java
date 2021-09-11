37bdadb01063188c63947066c7a08351b5612206
Shoaib Khan <shoaib.khan20@outlook.com>
2021-09-11 14:38:18 +0530

    Release 1.0.0

---
+++ src/main/java/net/shoaibkhan/cmbf/MainClass.java
@@ -0,0 +1,10 @@
+package net.shoaibkhan.cmbf;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MainClass implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		System.out.println("Hello Fabric world!");
+	}
+}
